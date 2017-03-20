c4a376d93c71cca23b0b0429195b2aaad251315e
alesiong <ypf7.10@hotmail.com>
2017-03-20 08:48:21 +0800

    change binary_pow to use bit op

--- src/math/binary_pow.cpp
+++ src/math/binary_pow.cpp
@@ -6,12 +6,12 @@ int64_t binary_pow(int64_t base, int exp) {
   if (exp < 0) return 0;  // TODO: raise error here
 
   int64_t result = 1;
-  while (exp != 0) {
-    if (exp % 2 == 1) {
+  while (exp) {     // exp !=0
+    if (exp & 1) {  // exp % 2 == 1
       result *= base;
     }
     base *= base;
-    exp /= 2;
+    exp >>= 1;  // exp /= 2
   }
   return result;
 }
