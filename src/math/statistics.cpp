b2b868c0da20ddc47de725a2a633a3cc7ee92ffd
alesiong <ypf7.10@hotmail.com>
2017-03-20 10:25:30 +0800

    add int statistic functions

--- src/math/statistics.cpp
+++ src/math/statistics.cpp
@@ -17,6 +17,7 @@ IMPLEMENT_SUM(double)
 #undef IMPLEMENT_SUM
 
 double mean(const double* arr, int size) { return sum(arr, size) / size; }
+double mean(const int* arr, int size) { return sum(arr, size) / size; }
 
 double variance(const double* arr, int size) {
   double sum_square = 0.0;
@@ -27,7 +28,19 @@ double variance(const double* arr, int size) {
   return sum_square / size - means * means;
 }
 
+double variance(const int* arr, int size) {
+  double sum_square = 0.0;
+  for (int i = 0; i < size; i++) {
+    sum_square += arr[i] * arr[i];
+  }
+  double means = mean(arr, size);
+  return sum_square / size - means * means;
+}
+
 double stddev(const double* arr, int size) {
+  return std::sqrt(variance(arr, size));
+}
+double stddev(const int* arr, int size) {
   return std::sqrt(variance(arr, size));
 }
 
@@ -40,8 +53,20 @@ double sample_variance(const double* arr, int size) {
   }
   return sum_square / (size - 1);
 }
+double sample_variance(const int* arr, int size) {
+  double sum_square = 0.0;
+  double means = mean(arr, size);
+  for (int i = 0; i < size; i++) {
+    double mean_diff = arr[i] - means;
+    sum_square += mean_diff * mean_diff;
+  }
+  return sum_square / (size - 1);
+}
 
 double sample_stddev(const double* arr, int size) {
+  return std::sqrt(sample_variance(arr, size));
+}
+double sample_stddev(const int* arr, int size) {
   return std::sqrt(sample_variance(arr, size));
 }
 
