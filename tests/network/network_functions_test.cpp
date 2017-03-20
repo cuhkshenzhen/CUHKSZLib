428b5c868f87c908488663251563ceac3b2248c1
Xiaoxing Ye <ye@xiaoxing.us>
2017-03-20 11:19:22 +0800

    readme: fix typo

--- tests/network/network_functions_test.cpp
+++ tests/network/network_functions_test.cpp
@@ -3,4 +3,5 @@
 
 TEST(NetworkFunctions, urlEncode) {
   ASSERT_EQ(cuhksz::urlEncode("http://google.com®"), "http%3A%2F%2Fgoogle.com%C2%AE");
+  ASSERT_EQ(cuhksz::urlEncode("Hello Günt+er"), "Hello%20G%C3%BCnt%2Ber");
 }
