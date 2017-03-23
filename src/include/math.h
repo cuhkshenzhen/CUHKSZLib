a64e72183b4e2561fff7750b7ddd4e3d808d7b25
alesiong <ypf7.10@hotmail.com>
2017-03-23 09:00:32 +0800

    rename math.h to

--- src/include/math.h
+++
@@ -1,9 +0,0 @@
-#ifndef CUHKSZ_MATH
-#define CUHKSZ_MATH
-
-#include "math/BigNum.h"
-#include "math/Fraction.h"
-#include "math/Matrix.h"
-#include "math/math_functions.h"
-
-#endif  // CUHKSZ_MATH
