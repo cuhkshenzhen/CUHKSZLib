6c78a7eb3a9036c880a3fba28ef17b0533c89105
Xiaoxing Ye <ye@xiaoxing.us>
2017-04-19 01:25:15 +0800

    use template class

--- src/include/zip/zip_functions.h
+++ src/include/zip/zip_functions.h
@@ -4,6 +4,8 @@
 
 #ifndef CUHKSZ_ZIP_ZIPFUNCTIONS
 #define CUHKSZ_ZIP_ZIPFUNCTIONS
+#include <iterator>
+#include <vector>
 namespace cuhksz {
 
 template<typename T>
@@ -12,8 +14,10 @@ class Zip {
   typedef std::vector<T> container;
 
   template<typename... Args>
-  Zip(const T &head, const Args &... args);
-
+  Zip(const T &head, const Args &... args):
+      items(head.size()), min_(head.size()) { // member initialization
+    zip_(head, args...);
+  }
   inline operator container() const {
     return items;
   };
@@ -25,9 +29,21 @@ class Zip {
  private:
 
   template<typename... Args>
-  void zip_(const T &head, const Args &... tail);
+  void zip_(const T &head, const Args &... tail){
+    if (head.size() < min_) min_ = head.size();
 
-  inline void zip_();
+    for (std::size_t i = 0; i < min_; ++i) {
+      typename T::const_iterator itr = head.begin();
+      std::advance(itr, i);
+      items[i].push_back(*itr);
+    }
+
+    return zip_(tail...);
+  };
+
+  void zip_(){
+    items.resize(min_);
+  }
 
   container items;
 
