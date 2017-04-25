bfe5097485a98aadd8a509644ebed41aed6c572b
Xiaoxing Ye <ye@xiaoxing.us>
2017-04-25 18:21:01 +0800

    add: json

--- samples/stringCast.cpp
+++ samples/stringCast.cpp
@@ -1,9 +1,8 @@
 #include "string_utils.h"
 #include <iostream>
-using namespace cuhksz;
 
 int main() {
   std::cout << "Casting string 12345 to ";
-  std::cout << typeid(stringCast<int>("12345")).name() << std::endl;
-  std::cout << "Result: " << stringCast<int>("12345");
+  std::cout << typeid(cuhksz::stringCast<int>("12345")).name() << std::endl;
+  std::cout << "Result: " << (int)cuhksz::stringCast<int>("12345");
 }
