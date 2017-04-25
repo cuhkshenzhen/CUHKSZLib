8b0fb94bec138450c496e2d45c932ffbfad7459f
alesiong <ypf7.10@hotmail.com>
2017-04-25 09:17:44 +0800

    add EnglishWords.txt & change cmaklists

--- samples/trie.cpp
+++ samples/trie.cpp
@@ -5,7 +5,13 @@
 #include "string_utils/Trie.h"
 
 int main() {
+  // Words are from https://github.com/dwyl/english-words
+  // preprocessed to remove words that have digits
   std::ifstream fin("EnglishWords.txt");
+  if (!fin) {
+    printf("Cannot open `EnglishWords.txt`!\n");
+    exit(1);
+  }
   std::string tmp;
 
   std::set<std::string> set;
@@ -16,9 +22,10 @@ int main() {
     trie.insert(tmp);
     set.insert(tmp);
     i++;
-    if (i % 10000 == 0) printf("Processed:%d\n", i);
+    if (i % 10000 == 0) printf("Processed: %d words\n", i);
   }
   for (auto& s : set) {
     if (!trie.contain(s)) std::cout << s << std::endl;
   }
+  printf("Above are the words that the Trie has problem with, if no, good!\n");
 }
