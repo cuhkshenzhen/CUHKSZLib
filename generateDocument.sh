21ab087f047e68b64b5131f8be4b7f83106880e8
alesiong <ypf7.10@hotmail.com>
2017-04-27 17:29:34 +0800

    [skip appveyor] test what's wrong with git push

--- generateDocument.sh
+++ generateDocument.sh
@@ -102,7 +102,7 @@ if [ -f "index.html" ]; then
     # Force push to the remote gh-pages branch.
     # The ouput is redirected to /dev/null to hide any sensitive credential data
     # that might otherwise be exposed.
-    git push --force "https://${GH_REPO_TOKEN}@${GH_REPO_REF}" > /dev/null 2>&1
+    git push --force "https://${GH_REPO_TOKEN}@${GH_REPO_REF}"# > /dev/null 2>&1
 else
     echo '' >&2
     echo 'Warning: No documentation (html) files have been found!' >&2
