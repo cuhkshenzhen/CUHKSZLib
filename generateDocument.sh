630b00998b028f60a0d6a6039c548268c1d80cb5
alesiong <ypf7.10@hotmail.com>
2017-04-28 11:22:06 +0800

    fix iv not found

--- generateDocument.sh
+++ generateDocument.sh
@@ -110,13 +110,14 @@ if [ -f "index.html" ]; then
 		ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 		ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 		ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-		openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in $TRAVIS_BUILD_DIR/deploy_key.enc -out deploy_key -d
+		# openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in $TRAVIS_BUILD_DIR/deploy_key.enc -out deploy_key -d
+		openssl aes-256-cbc -K $encrypted_c8affa483b8f_key -iv $encrypted_c8affa483b8f_iv -in $TRAVIS_BUILD_DIR/deploy_key.enc -out deploy_key -d
 		chmod 600 deploy_key
 		eval `ssh-agent -s`
 		ssh-add deploy_key
 
 		# Now that we're all set up, we can push.
-		git push $SSH_REPO gh-pages
+		git push --force $SSH_REPO gh-pages
 
 else
     echo '' >&2
