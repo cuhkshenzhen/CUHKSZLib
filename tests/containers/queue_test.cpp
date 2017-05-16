c7038e177e2b5ee646b68b60579dea5ee89967aa
alesiong <ypf7.10@hotmail.com>
2017-05-16 18:06:35 +0800

    format tests

--- tests/containers/queue_test.cpp
+++ tests/containers/queue_test.cpp
@@ -1,55 +1,49 @@
-#include "gtest/gtest.h"
-#include "containers/Queue.h"
 #include <queue>
+#include "containers/Queue.h"
+#include "gtest/gtest.h"
 
 cuhksz::Queue<int> testQueue;
 std::queue<int> stlQueue;
 
 TEST(queueTest, initialize) {
-    stlQueue.push(1);
-    cuhksz::Queue<int> initQueue1(stlQueue);
-    EXPECT_EQ(initQueue1.size(), 1);
+  stlQueue.push(1);
+  cuhksz::Queue<int> initQueue1(stlQueue);
+  EXPECT_EQ(initQueue1.size(), 1);
 }
 
 TEST(queueTest, typeConvert) {
-    auto queue1 = testQueue.toStlQueue();
-    std::queue<int> queue2 = testQueue;
-    EXPECT_EQ(queue1, queue2);
+  auto queue1 = testQueue.toStlQueue();
+  std::queue<int> queue2 = testQueue;
+  EXPECT_EQ(queue1, queue2);
 }
 
 TEST(queueTest, eqOperator) {
-    cuhksz::Queue<int> initQueue1(stlQueue);
-    testQueue = initQueue1;
-    EXPECT_EQ(testQueue.size(), 1);
+  cuhksz::Queue<int> initQueue1(stlQueue);
+  testQueue = initQueue1;
+  EXPECT_EQ(testQueue.size(), 1);
 }
 
 TEST(queueTest, enqueue) {
-    testQueue.enqueue(1);
-    testQueue.enqueue(2);
-    testQueue.enqueue(3);
-    EXPECT_EQ(testQueue.size(), 4);
+  testQueue.enqueue(1);
+  testQueue.enqueue(2);
+  testQueue.enqueue(3);
+  EXPECT_EQ(testQueue.size(), 4);
 }
 
-TEST(queueTest, first) {
-    EXPECT_EQ(testQueue.first(), 1);
-}
+TEST(queueTest, first) { EXPECT_EQ(testQueue.first(), 1); }
 
-TEST(queueTest, last) {
-    EXPECT_EQ(testQueue.last(), 3);
-}
+TEST(queueTest, last) { EXPECT_EQ(testQueue.last(), 3); }
 
 TEST(queueTest, dequeue) {
-    EXPECT_EQ(testQueue.dequeue(), 1);
-    EXPECT_EQ(testQueue.dequeue(), 1);
-    EXPECT_EQ(testQueue.first(), 2);
-    EXPECT_EQ(testQueue.size(), 2);
+  EXPECT_EQ(testQueue.dequeue(), 1);
+  EXPECT_EQ(testQueue.dequeue(), 1);
+  EXPECT_EQ(testQueue.first(), 2);
+  EXPECT_EQ(testQueue.size(), 2);
 }
 
 TEST(queueTest, clear) {
-    testQueue.clear();
-    EXPECT_TRUE(testQueue.isEmpty());
+  testQueue.clear();
+  EXPECT_TRUE(testQueue.isEmpty());
 }
 
-TEST(queueDeathTest, emptyDeath) {
-    EXPECT_DEATH(testQueue.dequeue(), "empty");
-}
+TEST(queueDeathTest, emptyDeath) { EXPECT_DEATH(testQueue.dequeue(), "empty"); }
--- tests/containers/vector_test.cpp
+++ tests/containers/vector_test.cpp
@@ -1,107 +1,100 @@
-#include "gtest/gtest.h"
-#include "containers/Vector.h"
-#include <vector>
 #include <iostream>
 #include <sstream>
+#include <vector>
+#include "containers/Vector.h"
+#include "gtest/gtest.h"
 #include "utils/error.h"
 
 cuhksz::Vector<int> testVector;
 std::vector<int> stlVector = {1, 2, 3, 4};
 
 TEST(vectorTest, initialize) {
-    cuhksz::Vector<int> initVector1(5, 1);
-    EXPECT_EQ(initVector1.size(), 5);
-    cuhksz::Vector<int> initVector2 = {1, 2, 1};
-    EXPECT_EQ(initVector2.size(), 3);
-    cuhksz::Vector<int> initVector3(stlVector);
-    EXPECT_EQ(initVector3.size(), 4);
+  cuhksz::Vector<int> initVector1(5, 1);
+  EXPECT_EQ(initVector1.size(), 5);
+  cuhksz::Vector<int> initVector2 = {1, 2, 1};
+  EXPECT_EQ(initVector2.size(), 3);
+  cuhksz::Vector<int> initVector3(stlVector);
+  EXPECT_EQ(initVector3.size(), 4);
 }
 
 TEST(vectorTest, typeConvert) {
-    auto vector1 = testVector.toStlVector();
-    std::vector<int> vector2 = testVector;
-    EXPECT_EQ(vector1, vector2);
+  auto vector1 = testVector.toStlVector();
+  std::vector<int> vector2 = testVector;
+  EXPECT_EQ(vector1, vector2);
 }
 
 TEST(vectorTest, eqOperator) {
-    cuhksz::Vector<int> initVector1(5, 1);
-    testVector = initVector1;
-    EXPECT_EQ(testVector.size(), 5);
+  cuhksz::Vector<int> initVector1(5, 1);
+  testVector = initVector1;
+  EXPECT_EQ(testVector.size(), 5);
 }
 
-TEST(vectorTest, get) {
-    EXPECT_EQ(testVector.get(1), 1);
-}
+TEST(vectorTest, get) { EXPECT_EQ(testVector.get(1), 1); }
 
-TEST(vectorTest, indexOperator) {
-    EXPECT_EQ(testVector[1], 1);
-}
-
+TEST(vectorTest, indexOperator) { EXPECT_EQ(testVector[1], 1); }
 
 TEST(vectorTest, push) {
-    testVector.push(1);
-    testVector.push(2);
-    testVector.push(3);
-    EXPECT_EQ(testVector.size(), 8);
+  testVector.push(1);
+  testVector.push(2);
+  testVector.push(3);
+  EXPECT_EQ(testVector.size(), 8);
 }
 
 TEST(vectorTest, erase) {
-    testVector.erase(5);
-    EXPECT_EQ(testVector[5], 2);
-    EXPECT_EQ(testVector.size(), 7);
+  testVector.erase(5);
+  EXPECT_EQ(testVector[5], 2);
+  EXPECT_EQ(testVector.size(), 7);
 }
 
 TEST(vectorTest, insert) {
-    testVector.insert(2, 4);
-    EXPECT_EQ(testVector[2], 4);
-    EXPECT_EQ(testVector.size(), 8);
+  testVector.insert(2, 4);
+  EXPECT_EQ(testVector[2], 4);
+  EXPECT_EQ(testVector.size(), 8);
 }
 
 TEST(vectorTest, set) {
-    testVector.set(2, 5);
-    EXPECT_EQ(testVector[2], 5);
-    EXPECT_EQ(testVector.size(), 8);
+  testVector.set(2, 5);
+  EXPECT_EQ(testVector[2], 5);
+  EXPECT_EQ(testVector.size(), 8);
 }
 
 TEST(vectorTest, pop) {
-    EXPECT_EQ(testVector.pop(), 3);
-    EXPECT_EQ(testVector.size(), 7);
+  EXPECT_EQ(testVector.pop(), 3);
+  EXPECT_EQ(testVector.size(), 7);
 }
 
 TEST(vectorTest, clear) {
-    testVector.clear();
-    EXPECT_TRUE(testVector.isEmpty());
+  testVector.clear();
+  EXPECT_TRUE(testVector.isEmpty());
 }
 
 TEST(vectorDeathTest, rangeDeath) {
-    EXPECT_DEATH(testVector.get(9), "range");
-    EXPECT_DEATH(testVector[10], "range");
-    EXPECT_DEATH(testVector.erase(8), "range");
-    EXPECT_DEATH(testVector.set(12, 1), "range");
+  EXPECT_DEATH(testVector.get(9), "range");
+  EXPECT_DEATH(testVector[10], "range");
+  EXPECT_DEATH(testVector.erase(8), "range");
+  EXPECT_DEATH(testVector.set(12, 1), "range");
 }
 
-TEST(vectorDeathTest, emptyDeath) {
-    EXPECT_DEATH(testVector.pop(), "empty");
-}
+TEST(vectorDeathTest, emptyDeath) { EXPECT_DEATH(testVector.pop(), "empty"); }
 
 TEST(vectorTest, istreamOperator) {
-    std::istringstream inputVector("{1, 2, 3, 5, 6, 7, 8}");
-    inputVector >> testVector;
-    EXPECT_EQ(testVector.size(), 7);
+  std::istringstream inputVector("{1, 2, 3, 5, 6, 7, 8}");
+  inputVector >> testVector;
+  EXPECT_EQ(testVector.size(), 7);
 }
 
 TEST(vectorTest, ostreamOperator) {
-    std::stringstream outVector;
-    outVector << testVector;
-    EXPECT_EQ(outVector.str(), "{1, 2, 3, 5, 6, 7, 8}");
+  std::stringstream outVector;
+  outVector << testVector;
+  EXPECT_EQ(outVector.str(), "{1, 2, 3, 5, 6, 7, 8}");
 }
 
 TEST(vectorDeathTest, istreamDeathTest) {
-    cuhksz::Vector<int> inputTestVector;
-    std::istringstream inputVector1("1, 2, 3");
-    std::istringstream inputVector2("{1, 2, 3");
-    std::istringstream inputVector3("{1, 2? 3}");
-    EXPECT_DEATH(inputVector1 >> inputTestVector, "startwith '\\{'");
-    EXPECT_DEATH(inputVector2 >> inputTestVector, "endwith");
-    EXPECT_DEATH(inputVector3 >> inputTestVector, "Unexpected character");
+  cuhksz::Vector<int> inputTestVector;
+  std::istringstream inputVector1("1, 2, 3");
+  std::istringstream inputVector2("{1, 2, 3");
+  std::istringstream inputVector3("{1, 2? 3}");
+  EXPECT_DEATH(inputVector1 >> inputTestVector, "startwith '\\{'");
+  EXPECT_DEATH(inputVector2 >> inputTestVector, "endwith");
+  EXPECT_DEATH(inputVector3 >> inputTestVector, "Unexpected character");
 }
