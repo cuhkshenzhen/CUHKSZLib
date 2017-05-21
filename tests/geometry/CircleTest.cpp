c0419e6ea20440844054efe81317c5dd62a119b5
Xuan Huang <115010159@link.cuhk.edu.cn>
2017-05-21 17:44:15 +0800

    add CircleTest.cpp

---
+++ tests/geometry/CircleTest.cpp
@@ -0,0 +1,23 @@
+#include "geometry/Circle.h"
+
+#include "gtest/gtest.h"
+
+TEST(Circle, initialize) {
+    cuhksz::Circle a, b(2), c(cuhksz::Point<2>(2, 3));
+    cuhksz::Circle d(cuhksz::Point<2>(2, 3), 2);
+}
+
+TEST(Circle, getArea) {
+    cuhksz::Circle a(cuhksz::Point<2>(2, 3), 2);
+    EXPECT_DOUBLE_EQ(a.getArea(), 3.1415926 * 4);
+}
+
+TEST(Circle, getPerimeter) {
+    cuhksz::Circle a(cuhksz::Point<2>(2, 3), 3);
+    EXPECT_DOUBLE_EQ(a.getPerimeter(), 3.1415926 * 6);
+}
+
+TEST(Circle, getPoint) {
+    cuhksz::Circle a(2);
+    EXPECT_EQ(a.getPoint(3.1415926/4), cuhksz::Point<2>(std::sqrt(2), std::sqrt(2)));
+}
